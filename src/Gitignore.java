d5f41fbeb6817f8931517fe83c7a84bb89f9fca8
Abdullah Kadioglu <abdkadioglu21@gmail.com>
2022-08-16 23:10:06 +0300

    C.1

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -1,5 +1,7 @@
 public class Gitignore {
     public static void main(String[] args) {
         System.out.println("git ignore deneme 1");
+        System.out.println("git ignore deneme 2");
+        System.out.println("git ignore deneme 3 master");
     }
 }
